6d643a86d29d2cf6cff406b090dd552877cd4292
Jc-hammond <jasonchammond@gmail.com>
2021-08-23 23:59:39 -0600

    sum

--- CodeWars.playground/Contents.swift
+++ CodeWars.playground/Contents.swift
@@ -14,3 +14,20 @@ func boolToWord(_ bool: Bool) -> String {
 
 boolToWord(true)
 boolToWord(false)
+
+
+//It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+// Return the average of the given array rounded down to its nearest integer
+// The array will never be empty.
+
+func getAverage(_ marks: [Int]) -> Int {
+    
+    var sum = 0
+    
+    for (i, el) in marks.enumerated() {
+        sum += el        
+    }
+    return sum / marks.count
+    
+}
+ getAverage([1,2,3,4,5,6])
